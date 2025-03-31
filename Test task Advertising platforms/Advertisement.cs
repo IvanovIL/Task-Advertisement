a94c9b54348ee09a1bdb506b4927955ff951d388
Stefan Flyagin <ila723483@gmail.com>
2025-03-31 11:02:27 +0300

    Add project files.

---
+++ Test task Advertising platforms/Advertisement.cs
@@ -0,0 +1,11 @@
+namespace Test_task_Advertising_platforms
+{
+    public class Advertisement
+    {
+        public int Id {  get; set; }
+
+        public string Name { get; set; }
+
+        public string[] DomenName { get; set; }
+    }
+}
---
+++ Test task Advertising platforms/Controllers/AdvertisementController.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Test_task_Advertising_platforms;
+
+namespace Test_task_Advertising_platforms.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdvertisementController : ControllerBase
+    {
+
+        private static List<Advertisement> AdvertisementLists = new List<Advertisement>();
+        private readonly IConfiguration _configuration;
+
+
+        private readonly ILogger<AdvertisementController> _logger;
+
+        public AdvertisementController(ILogger<AdvertisementController> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Загружает из файла json данные рекламы
+        /// </summary>
+        [HttpGet]
+        [Route("Загрузка данных рекламных площадок")]
+        public async Task<IActionResult> Upload()
+        {
+            string path = _configuration.GetSection("PathInJson:Json").Value;
+
+            string file = "";
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                file = await streamReader.ReadToEndAsync();
+            }
+
+            AdvertisementLists = JsonConvert.DeserializeObject<List<Advertisement>>(file);
+
+            return Ok("Данные загружены");
+        }
+
+        /// <summary>
+        /// Находит рекламные площадки по локациям
+        /// </summary>
+        /// <param name="NameDomen">Наименование локации</param>
+        [HttpPost]
+        [Route("Найти рекламу по локации")]
+        public async Task<List<Advertisement>> FindAdvertisementAsync(string NameDomen)
+        {
+            List<Advertisement> advertisements = new List<Advertisement>();
+
+            int id = 0;
+
+            NameDomen = NameDomen.ToLower();
+
+            if (NameDomen.StartsWith('/') == false)
+            {
+                NameDomen = "/" + NameDomen;
+            }
+
+            foreach (var advertisement in AdvertisementLists)
+            {
+                foreach (var item in advertisement.DomenName)
+                {
+
+                    if (item.Length <= NameDomen.Length)
+                    {
+
+                        if (NameDomen.EndsWith("/ru") | item.EndsWith("/ru"))
+                        {
+                            advertisements.Add(advertisement);
+                        }
+                        else if(item.Equals(NameDomen))
+                        {
+                            advertisements.Add(advertisement);
+                        }
+                        else if (NameDomen.Contains(item))
+                        {
+                            advertisements.Add(advertisement);
+                        }
+
+
+                    }
+                }
+
+            }
+
+            return advertisements;
+
+        }
+    }
+}
